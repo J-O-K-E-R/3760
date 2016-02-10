8b8f2a3b578159750e02ad3adc90fe7c6d9d8b93
Justin <tribekiller911@gmail.com>
2016-02-10 09:19:18 -0800

    lab 5

---
+++ Lab5/src/lab5/Lab5.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5;
+
+/**
+ *
+ * @author Justin
+ */
+public class Lab5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int array[] = {2,5,8,3,6,9,1,6,5};
+        System.out.println(array.length);
+    }
+    public int maxArray(int s[]) {
+        int pos = 0;
+        int max = 0;
+        
+        
+        
+        return pos;
+    }
+    
+}
